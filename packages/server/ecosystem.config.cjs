c44832121b7fdecf21c7b953f8861c2aa792b65d
anson <anson@ricequant.com>
2022-04-14 15:06:09 +0800

    feat(server): support production mode

---
+++ packages/server/ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "factor-monitor",
+      script: "./src/index.js",
+      instances: "max",
+      max_memory_restart: "300M",
+      exp_backoff_restart_delay: 100,
+      merge_logs: true,
+      time: true,
+      error_file: "./log/pm2-error.log",
+      out_file: "./log/pm2-out.log",
+    },
+  ],
+};
