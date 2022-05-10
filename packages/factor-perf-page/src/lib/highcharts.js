02a3ec51fcbf07b14cce89fc2fefe79d24b27ac2
anson <anson@ricequant.com>
2022-05-10 14:14:45 +0800

    perf(page): shrink bundle size

--- packages/factor-perf-page/src/lib/highcharts.js
+++ packages/factor-perf-page/src/lib/highcharts.js
@@ -1,13 +1,14 @@
 import Highcharts from "highcharts/highstock";
 import gridLight from "highcharts/themes/grid-light";
 import darkUnica from "highcharts/themes/dark-unica";
-import accessibility from "highcharts/modules/accessibility";
 
 gridLight(Highcharts);
-accessibility(Highcharts);
 Highcharts.setOptions({
   chart: {
     height: 500,
+  },
+  accessibility: {
+    enabled: false,
   },
   credits: {
     enabled: false,
